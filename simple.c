77971be06b6a5cfe62f0e2ae7bc610799d005d98
Brandon Wong <bwong7@hawaii.edu>
2018-09-19 16:52:53 -1000

    Added simple program

---
+++ simple.c
@@ -0,0 +1,11 @@
+/* This is a simple C program that needs contributors to add to it */
+
+#include <stdio.h>
+
+int main() 
+{
+	printf("First Contribution\n");
+
+	/* Please add something here */
+}
+
